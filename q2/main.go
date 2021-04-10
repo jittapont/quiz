1c20e4e77d158017d8b9587c8c27629864308b2b
jittapon <jittapon.t@gmail.com>
2021-04-10 21:55:08 +0700

    feat: add new method to group by word and improve time complexity

--- q2/main.go
+++ q2/main.go
@@ -30,8 +30,29 @@ func groupWords(words []string) [][]string {
 	return groups
 }
 
+func groupWords2(words []string) [][]string {
+	wordGroupMapper := make(map[[26]int][]string)
+	groups := make([][]string, 0)
+	for _, word := range words {
+		arr := [26]int{}
+		for _, char := range word {
+			arr[int(char) - int('A')] += 1
+		}
+		group, ok := wordGroupMapper[arr]
+		if !ok {
+			wordGroupMapper[arr] = append(make([]string, 0), word)
+		} else {
+			wordGroupMapper[arr] = append(group, word)
+		}
+	}
+	for _, group := range wordGroupMapper {
+		groups = append(groups, group)
+	}
+	return groups
+}
+
 func main() {
 	words := []string{"VMRCO", "VORCM", "MCRTOX", "ZXTAC", "XZATC", "XMTCOR", "OXVS", "AZTXC", "VXOS", "RZAT", "MRCOTX", "SVXO", "TRAZ", "ZTAR", "MVOCR"}
-	groups := groupWords(words)
+	groups := groupWords2(words)
 	fmt.Println(groups)
 }
--- q2/main_test.go
+++ q2/main_test.go
@@ -59,3 +59,35 @@ func Test_groupWords(t *testing.T) {
 	}
 
 }
+
+func Test_groupWords2(t *testing.T) {
+	tests := []struct {
+		name  string
+		words []string
+		want  [][]string
+	}{
+		{
+			name:  "When we provide a words, it should return correct group",
+			words: []string{"VMRCO", "VORCM", "MCRTOX", "ZXTAC", "XZATC", "XMTCOR", "OXVS", "AZTXC", "VXOS", "RZAT", "MRCOTX", "SVXO", "TRAZ", "ZTAR", "MVOCR"},
+			want:  [][]string{{"MCRTOX", "XMTCOR", "MRCOTX"}, {"OXVS", "VXOS", "SVXO"}, {"RZAT", "TRAZ", "ZTAR"}, {"VMRCO", "VORCM", "MVOCR"}, {"ZXTAC", "XZATC", "AZTXC"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := groupWords2(tt.words)
+			sort.Slice(got, func(i, j int) bool {
+				if len(got[i]) == 0 && len(got[j]) == 0 {
+					return false
+				}
+				if len(got[i]) == 0 || len(got[j]) == 0 {
+					return len(got[i]) == 0
+				}
+				return got[i][0] < got[j][0]
+			})
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupWords() result mismatch got : %v, want : %v", got, tt.want)
+			}
+		})
+	}
+
+}
